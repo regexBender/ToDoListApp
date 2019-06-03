2fb7f38f44bb398f88360b66c55422294471a3f6
Alec Landow <alandow@webassign.net>
2019-06-03 00:25:28 -0400

    Trying to get checkbox to change the todo item

--- my-app/src/ToDoList.js
+++ my-app/src/ToDoList.js
@@ -26,37 +26,7 @@ class ToDoList extends React.Component {
     componentWillMount() {
         this.setState({
             userid: this.props.match.params.id
-        });
-/*
-        const jwt = localStorage.getItem("JWT");
-
-        if (!jwt) {
-            this.props.history.push('/login');
-        }
-
-        axios.get(`/authUser/${this.props.match.params.id}`, // Look up async await to prevent flash
-            { headers: {
-                Authorization: `Bearer ${jwt}`
-            }})
-            .then( (res) => {
-                if (res) {
-                    console.log("authenticated");
-                    this.setState({
-                        authorized: true
-                    })
-                } else {
-                    localStorage.removeItem("JWT");
-                    console.log("authentication failed");
-                    this.props.history.push('/login');
-                }
-            })
-            
-            .catch( (err) => {
-                localStorage.removeItem("JWT");
-                console.log("There was an error2 " + err);
-                this.props.history.push('/login');
-            })
-     */       
+        });      
     }
 
     componentDidMount() {
@@ -110,26 +80,6 @@ class ToDoList extends React.Component {
                 console.log("There was an error2 " + err);
                 this.props.history.push('/login');
             })
-/*
-            axios.get(`/todos/${this.props.match.params.id}`)
-                .then( (res) => {
-                    if (res && this._isMounted) {
-                        this.setState( () => ({
-                            todoData: res.data
-                        }), () => {
-                            if (this._isMounted && this.state.todoData) {
-                                this.renderTodos();
-                                //window.addEventListener('load', this.renderTodos)
-                            }
-                        });                     
-                    } else {
-                        console.log("Res was null or undef: " + JSON.stringify(res) );
-                    }
-                })
-                .catch( (err) => {
-                    console.log("There was an error3 " + err);
-                })
-                */
         
     }
 
@@ -143,18 +93,46 @@ class ToDoList extends React.Component {
     }
 
     wrapTodo = (item, index) => {
-        item = <div key={item.id}
-                className={(index % 2 == 0) ? "task" : "task2"}>
-                <input type="checkbox" defaultChecked={item.checked ? 1 : 0}/>
+        let id = item.id;
+        let itemDiv = <div key={item.id} id={item.id}
+                className = {
+                    (index % 2 == 0 ? "task" : "task2")
+                    +
+                    (item.checked ? " complete" : "")
+                    }>
+                <input id={"checkbox_" + item.id}
+                    type="checkbox" 
+                    defaultChecked={item.checked ? 1 : 0}
+                    onChange={ 
+                        this.updateCheckbox.bind(this, id)
+                    }
+                    
+                />
                 {item.content}
             </div>;
 
-        return item;
+        return itemDiv;
     }
+
+
+    updateCheckbox = (id) => {
+        console.log("checkbox_" + id);
+        let todoCheckbox = document.getElementById("checkbox_" + id);
+        todoCheckbox.checked = !todoCheckbox.checked;
+
+        let todoDiv = document.getElementById(id);
+        todoDiv.className = todoCheckbox.checked ?
+            todoDiv.className + " complete" :
+            todoDiv.className.replace(" complete", "");
+
+        this.forceUpdate();
+    }
+  
+    
 
     renderTodos = () => {
         console.log("_isMounted: " + this._isMounted);
-        console.log("todoData: " + this.state.todoData);
+        //console.log("todoData: " + this.state.todoData);
 
         if (this._isMounted && this.state.todoData && this.state.authorized) {
             let listContainer = document.getElementById("todolist");
@@ -166,7 +144,7 @@ class ToDoList extends React.Component {
             
             //console.log("List Container: " + listContainer);
             
-            ReactDOM.render(displayTodos, listContainer); 
+            ReactDOM.render(displayTodos.reverse(), listContainer); 
         }
     }
 
@@ -181,7 +159,7 @@ class ToDoList extends React.Component {
         axios.post("/todos", qs.stringify(newItem), config)
         .then( (res) => {
             if (res) {
-                console.log(JSON.stringify(res.data));
+                //console.log(JSON.stringify(res.data));
                 
                 this.setState( (state) => {
                     state.todoData.push(res.data);
@@ -192,7 +170,7 @@ class ToDoList extends React.Component {
                 })
                 
             
-                console.log(JSON.stringify(res.data));
+                //console.log(JSON.stringify(res.data));
                 
             } else {
                 console.log("Res was null or undef");
@@ -206,7 +184,7 @@ class ToDoList extends React.Component {
     }
 
     render() {
-        console.log(this.state.todoData);
+        //console.log(this.state.todoData);
         if (this._isMounted && this.state.todoData && this.state.authorized) {
             return (
                 <div className="ToDoList">
@@ -232,7 +210,11 @@ class ToDoList extends React.Component {
                                     id="add_item" 
                                     name="add_item" 
                                     placeholder="Add an item." />
-                                <input className="submit_task" type="submit" value="+" />
+                                <input 
+                                    className="submit_task"
+                                    type="submit" 
+                                    disabled={!this.newItemContent}
+                                    value="+" />
                             </form>
                         </div>
                     <div className="main_container">
